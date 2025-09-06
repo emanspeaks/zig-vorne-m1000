9dc3f951d87f86fc488217fcce5e3dbfe7ee70ff
Randy Eckman <emanspeaks@gmail.com>
2025-09-06 18:28:37 -0500

    more lua updates for debugging

--- vlc/multicast_vlc_status.lua
+++ vlc/multicast_vlc_status.lua
@@ -12,7 +12,7 @@ function descriptor()
         url = "https://github.com/emanspeaks/zig-vorne-m1000",
         shortdesc = "Broadcasts filename + playhead over multicast",
         description = "Pushes current VLC playback status as UDP multicast JSON",
-        capabilities = {}
+        capabilities = {"input-listener"}  -- Add input listener capability
     }
 end
 
@@ -21,21 +21,87 @@ local mcast_addr = "239.255.0.1"
 local mcast_port = 5005
 local running = false
 
+-- Add menu item to VLC interface
+function menu()
+    return {"Start Broadcasting", "Stop Broadcasting", "Send Test Message"}
+end
+
+-- Handle menu selections
+function trigger_menu(id)
+    if id == 1 then
+        msg.info("[multicast_time] Manual start requested")
+        if not running then
+            activate()
+        end
+    elseif id == 2 then
+        msg.info("[multicast_time] Manual stop requested")
+        deactivate()
+    elseif id == 3 then
+        msg.info("[multicast_time] Manual test requested")
+        test_multicast()
+    end
+end
+
+-- Input listener functions
+function input_changed()
+    msg.info("[multicast_time] Input changed - media loaded")
+    if not running then
+        msg.info("[multicast_time] Auto-starting due to media load")
+        activate()
+    end
+end
+
+function playing_changed(state)
+    msg.info("[multicast_time] Playback state changed: " .. tostring(state))
+    if state == 1 then  -- playing
+        msg.info("[multicast_time] Media started playing")
+    elseif state == 0 then  -- stopped/paused
+        msg.info("[multicast_time] Media stopped/paused")
+    end
+end
+
+-- VLC startup function
+function vlc_main()
+    msg.info("[multicast_time] VLC main called - extension loaded")
+    -- Don't auto-activate here, wait for user interaction or media events
+end
+
 -- Activate when user enables extension
 function activate()
     msg.info("[multicast_time] ACTIVATING EXTENSION...")
+    
+    if running then
+        msg.info("[multicast_time] Already running, skipping activation")
+        return
+    end
+    
     udp = vlc.net.udp_socket()
     if not udp then
         msg.err("[multicast_time] FAILED to create UDP socket!")
         return
     end
+    
     -- Set multicast TTL so packets can travel across network
     vlc.net.set_multicast_ttl(udp, 10)
     msg.info("[multicast_time] UDP socket created, TTL set to 10")
+    
     running = true
     msg.info("[multicast_time] Starting timer with 250ms interval")
-    vlc.timer.register(250, push_status)  -- every 250ms
+    
+    -- Register the timer
+    local timer_id = vlc.timer.register(250, push_status)
+    if timer_id then
+        msg.info("[multicast_time] Timer registered successfully (ID: " .. tostring(timer_id) .. ")")
+    else
+        msg.err("[multicast_time] Failed to register timer!")
+        running = false
+        return
+    end
+    
     msg.info("[multicast_time] Extension activated successfully!")
+    
+    -- Send an initial test message
+    test_multicast()
 end
 
 -- Deactivate on exit
